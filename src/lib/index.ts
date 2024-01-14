3e66970f36d70e61cad35224225d68eeeca9adcf
MyLastSerenade <ssakanek86@googlemail.com>
2024-01-14 17:28:28 +0100

    some cleaning up

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,41 +2,6 @@ import ytdl from 'ytdl-core'
 import ffmpeg from 'fluent-ffmpeg'
 import ffmppegPath from '@ffmpeg-installer/ffmpeg'
 import { Writable } from 'stream';
-import { progression } from './store';
-
-/* export async function getAudio(url: string) {
-    let title = "";
-    if (ffmppegPath.path) {
-        ffmpeg.setFfmpegPath(ffmppegPath.path)
-        let stream = ytdl(String(url), { filter: 'audioonly' });
-        title = (await ytdl.getBasicInfo(String(url))).videoDetails.title.replaceAll('"', '');
-        let start = Date.now();
-        let status;;
-        const request = new Promise<string>((resolve, reject) => {
-            ffmpeg(stream)
-                .audioBitrate(128)
-                .save(`audio/${title}.mp3`)
-                .on('progress', p => {
-                    process.stdout.write(`${p.targetSize}kb downloaded`);
-
-                })
-                .on('end', () => {
-                    console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-                    //progression.update((p) => p = "done");
-                    status = Promise.resolve()
-                    resolve(title)
-                })
-                .on('error', (err) => {
-                    console.error('Error during processing:', err);
-                    reject(err); // Reject the promise in case of an error
-                });
-        })
-        let res = await request;
-        return res
-    }
-    return null
-}
- */
 
 export async function getAudio(url: string) {
     let title = "";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import { applyAction, enhance } from '$app/forms';
-	import { page } from '$app/stores';
-	import { progression } from '$lib/store';
-	import { json, type SubmitFunction } from '@sveltejs/kit';
 	import Loading from '../components/Loading.svelte';
 
 	let url = $state(null);
@@ -24,38 +20,26 @@
 		const arrayBuffer = await res.arrayBuffer();
 		const blob = new Blob([arrayBuffer], { type: 'audio/mpeg' });
 
-		// Create a download link
 		const downloadLink = document.createElement('a');
 		downloadLink.href = URL.createObjectURL(blob);
 		downloadLink.download = `${fileName}`;
-
-		// Trigger a click on the link to start the download
 		downloadLink.click();
+
 		loading = false
 	}
 
 	// https://www.youtube.com/watch?v=kFU7nRe7C0A
 </script>
 
-
 <div class="relative pt-12">
 	<div class="absolute left-[20%] z-10 h-[240px] w-[240px] rounded-full bg-purple-600 blur-3xl" />
 	<div class="relative z-20 flex max-w-[400px] flex-col items-center gap-4">
 		<p class="text-center text-lg">Please copy the Url to the Youtube-Video into this field:</p>
-		<input
-			name="url"
-			class="form-field"
-			type="text"
-			placeholder="URL"
-			bind:value={url}
-			
-		/>
-		<!-- on:input={(event) => changeUrl(event)} -->
+		<input name="url" class="form-field" type="text" placeholder="URL" bind:value={url} />
 		<button
 			type="button"
 			class="w-[120px] rounded-lg border-2 border-gray-100 bg-transparent text-lg text-white"
-			on:click={() => fetchAudio()}
-			>Send it...</button
+			on:click={() => fetchAudio()}>Send it...</button
 		>
 		{#if loading}
 			<div class="mr-6 h-[50px] w-[50px]">
